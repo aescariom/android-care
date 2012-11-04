02a5ce87b217381b22d12132e69cea05d362f3e2
alejandro@escario.org <alejandro@escario.org@898f9fcd-03bc-4f22-bd80-c159e4617009>
2012-11-04 21:45:10 +0000

    Eliminados los readme

--- AndroidCare_web/src/org/androidcare/web/server/api/RetrieveReminders.java
+++ AndroidCare_web/src/org/androidcare/web/server/api/RetrieveReminders.java
@@ -1,19 +1,3 @@
-/** 
- * This library/program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either 
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library/program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public 
- * License along with this library/program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- */
-
 package org.androidcare.web.server.api;
 
 import java.io.*;
