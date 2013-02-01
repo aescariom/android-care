bf1a1a795a48420a86936d4d7f81d3c815f01f23
alejandro@escario.org <alejandro@escario.org@898f9fcd-03bc-4f22-bd80-c159e4617009>
2013-02-01 15:28:49 +0000

    Añadidos criterios de selección del sistema de localización

--- AndroidCare_android/src/org/androidcare/android/service/location/LocationService.java
+++ AndroidCare_android/src/org/androidcare/android/service/location/LocationService.java
@@ -4,6 +4,7 @@ import org.androidcare.android.service.ConnectionService;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -37,10 +38,21 @@ public class LocationService extends ConnectionService {
         int result = super.onStartCommand(intent, flags, startId);
         Log.i(tag, "Location service started");
 
+        // setting the criteria
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(true);
+
         // Acquire a reference to the system Location Manager
         this.locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        String bestProvider = locationManager.getBestProvider(criteria, true);
         // Register the listener with the Location Manager to receive location updates
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, this.minSeconds,
+        locationManager.requestLocationUpdates(bestProvider, this.minSeconds,
                 this.minDistance, locationListener);
         
         return result;
