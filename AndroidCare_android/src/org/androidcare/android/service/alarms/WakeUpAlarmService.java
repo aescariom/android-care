56d911ba560de7104ed853273ea320ee8af514f4
alvarodelrosal <alvarodelrosal@898f9fcd-03bc-4f22-bd80-c159e4617009>
2014-04-21 18:54:45 +0000

    las alarmas de despertador a veces no se inicializan bien...

--- AndroidCare_android/src/org/androidcare/android/service/alarms/WakeUpAlarmService.java
+++ AndroidCare_android/src/org/androidcare/android/service/alarms/WakeUpAlarmService.java
@@ -9,14 +9,9 @@ import org.androidcare.android.alarms.Alarm;
 import org.androidcare.android.service.GravitySensorRetriever;
 
 import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class WakeUpAlarmService extends AlarmService {
 
-    private PowerManager.WakeLock wakeLock;
-    private GravitySensorRetriever gravitySensorRetriever;
-    private Timer timer = new Timer();
     static final int UPDATE_INTERVAL = 500;
     static final float DELTA = 2.5f;
     private WakeUpAlarmService thisService = this;
@@ -30,74 +25,97 @@ public class WakeUpAlarmService extends AlarmService {
 
     @Override
     public void onCreate() {
-        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-        wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "My wakelook");
-
-        gravitySensorRetriever = new GravitySensorRetriever(this);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
+
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "My wakelook");
         wakeLock.acquire();
 
         Bundle bundle = intent.getExtras();
         super.setAlarm((Alarm) bundle.getSerializable("alarm"));
 
-        runWatchDog();
+        final GravitySensorRetriever gravitySensorRetriever = new GravitySensorRetriever(this);
+
+        runWatchDog(gravitySensorRetriever, wakeLock);
 
         return START_STICKY;
     }
 
-    private void runWatchDog() {
-        detectMovement();
+    private void runWatchDog(final GravitySensorRetriever gravitySensorRetriever, PowerManager.WakeLock wakeLock) {
+        Log.e("TEST", "EMPEZAMOS A MONITORIZAR");
 
-        if (mustLaunchAlarmByTime()) {
-            Log.e("TEST", "LANZAMOS ALARMA");
+        while (true)  {
+            detectMovement(gravitySensorRetriever, wakeLock);
+
+            if (mustLaunchAlarmByTime()) {
+                launchAlarm(gravitySensorRetriever, wakeLock);
+            }
+
+            try {
+                Thread.sleep(UPDATE_INTERVAL);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private void launchAlarm(GravitySensorRetriever gravitySensorRetriever, PowerManager.WakeLock wakeLock) {
+        try {
+            wakeLock.release();
+        } catch (RuntimeException ex) {
             /*
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    abstractInitiateAlarm();
-                }
-            }).start();
-            */
+                Pues sí, otra chapu, pero se vuelve loco con el contador de locks y lanza excepciones. Ver más detalle:
+                http://stackoverflow.com/questions/12140844/java-lang-runtimeexception-wakelock-under-locked-c2dm-lib
+             */
+        }
+        gravitySensorRetriever.stopSensorsUpdate();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+               abstractInitiateAlarm();
+            }
+        }).start();
+
+        thisService.stopSelf();
+    }
+
+    private void detectMovement(final GravitySensorRetriever gravitySensorRetriever, PowerManager.WakeLock wakeLock) {
+        float[] sensorsData = gravitySensorRetriever.getSensorsData();
+
+        Log.e("TEST", "estado if sensores: " + lastX + "; " + lastY + "; " + lastZ);
+        if (lastX != 0 && lastY != 0 && lastZ != 0 ) {
+            if (isOutOfinterval(sensorsData)) {
+                Log.e("TEST", "Movimiento encontrado :)");
+                gravitySensorRetriever.stopSensorsUpdate();
+                wakeLock.release();
+                thisService.stopSelf();
+            }
+        }
+
+        Log.e("TEST", "Una asignacion feliz: " + sensorsData[0] + "; " + sensorsData[1] + "; " + sensorsData[2]);
+        if (sensorsData != null) {
+            lastX = sensorsData[0];
+            lastY = sensorsData[1];
+            lastZ = sensorsData[2];
+        } else {
             thisService.stopSelf();
+            //Lo matamos porque no disponemos de datos... :(
         }
     }
 
-    private void detectMovement() {
-        Log.e("TEST", "EMPEZAMOS A MONITORIZAR");
+    private boolean isOutOfinterval(float[] sensorsData) {
+        Log.e("TEST", "Unos sensores: " + sensorsData[0] + "; " + sensorsData[1] + "; " + sensorsData[2]);
+        Log.e("TEST", "Avg sensores: " + lastX + "; " + lastY + "; " + lastZ);
 
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                float[] sensorsData = gravitySensorRetriever.getSensorsData();
-
-                if (lastX != 0 && lastY != 0 && lastZ != 0 ) {
-                    if (isOutOfinterval(sensorsData)) {
-                        Log.e("TEST", "Movimiento encontrado :)");
-                        thisService.stopSelf();
-                    }
-                }
-
-                if (sensorsData != null) {
-                    lastX = sensorsData[0];
-                    lastY = sensorsData[1];
-                    lastZ = sensorsData[2];
-                }
-            }
-
-            private boolean isOutOfinterval(float[] sensorsData) {
-                Log.e("TEST", "Unos sensores: " + sensorsData[0] + "; " + sensorsData[1] + "; " + sensorsData[2]);
-                Log.e("TEST", "Avg sensores: " + lastX + "; " + lastY + "; " + lastZ);
-
-                return lastX + DELTA < sensorsData[0] || lastX - DELTA > sensorsData[0] ||
-                        lastY + DELTA < sensorsData[1] || lastY - DELTA > sensorsData[1] ||
-                        lastZ + DELTA < sensorsData[2] || lastZ - DELTA > sensorsData[2];
-            }
-
-        }, 0, UPDATE_INTERVAL);
+        return lastX + DELTA < sensorsData[0] || lastX - DELTA > sensorsData[0] ||
+                lastY + DELTA < sensorsData[1] || lastY - DELTA > sensorsData[1] ||
+                lastZ + DELTA < sensorsData[2] || lastZ - DELTA > sensorsData[2];
     }
 
     private boolean mustLaunchAlarmByTime() {
@@ -109,13 +127,6 @@ public class WakeUpAlarmService extends AlarmService {
 
     private boolean isNowAfterEndTime(Date now, Date endTime) {
         return now.compareTo(endTime) > 0;
-    }
-
-    @Override
-    public void onDestroy() {
-        timer.cancel();
-        gravitySensorRetriever.stopSensorsUpdate();
-        wakeLock.release();
     }
 
     @Override
